98caa9b6040566e75c4edf04e8574bbed863af88
Utkarsh Sharma <Vishal30982@gmail.com>
2023-12-09 21:58:03 +0530

    Added color picker to select any theme of card

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 function ChangeTheme (theme) {
     document.querySelector(':root').style.setProperty('--theme', `${theme}`);
     localStorage.setItem('theme', document.querySelector(':root').style.getPropertyValue('--theme'))
-    // console.log(document.querySelector(':root').style.getPropertyValue('--theme'))
 }
 let choosenTheme = localStorage.getItem('theme');
 document.querySelector(':root').style.setProperty('--theme', choosenTheme);
